68e243eaa65b6eefcf6a8ce6192880496ef1a079
kepler4567 <kepler.batista456@gmail.com>
2024-09-07 12:01:11 -0300

    first commit

---
+++ src/trabalhoMOA/Item.java
@@ -0,0 +1,36 @@
+package trabalhoMOA;
+
+public class Item {
+
+	private Integer weight;
+	private Integer value;
+	
+	public Item() {
+	}
+
+	public Item(Integer weight, Integer value) {
+		this.weight = weight;
+		this.value = value;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	
+	public double getValueWeightRatio() {
+        // Evitar divisão por zero
+        return (double) value / (double) weight;
+    }
+}
